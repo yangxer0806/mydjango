ba91b92a6dba8c733aebfa1ab603ec7576f529d2
Unknown <xe_young@163.com>
2020-05-17 11:19:52 +0800

    学习代码

--- apitest/apps/cpaasapi/models.py
+++ apitest/apps/cpaasapi/models.py
@@ -10,3 +10,33 @@ class PaaS(models.Model):
     playTimes = models.IntegerField()
     playDelay = models.IntegerField()
     data = models.TextField()
+
+# 接口信息
+class api_info(models.Model):
+    # 接口ID
+    id = models.IntegerField(primary_key=True)
+    # 接口名称
+    api_name = models.CharField(max_length=20)
+    # 接口所属产品
+    api_product = models.IntegerField()
+    # URL
+    api_url = models.CharField(max_length=200)
+
+# 产品信息
+class product(models.Model):
+    # 产品ID
+    id = models.IntegerField(primary_key=True)
+    # 产品名称
+    name = models.CharField(max_length=50)
+
+
+# 接口参数
+class api_param(models.Model):
+    # 参数ID
+    id = models.IntegerField(primary_key=True)
+    # 接口ID
+    api_id = models.IntegerField()
+    # 参数名
+    param = models.CharField(max_length=50)
+    # 上级参数
+    f_param = models.CharField(max_length=50)
--- apitest/apps/cpaasapi/views.py
+++ apitest/apps/cpaasapi/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+
+
+
 
 # Create your views here.
 def index(request):
@@ -17,4 +21,5 @@ def login(request):
             name = request.GET.get('name')
         else:
             name = 'EveryOne'
-        return HttpResponse('username is ' + name)
+        # return HttpResponse('username is ' + name)
+        return render(request, 'index.html', context={'title':'测试平台','name' : name})
